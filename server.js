d2ef3bed663e924447a10adec36bc84196efb48f
l4te-fl4ker <136440740+l4te-fl4ker@users.noreply.github.com>
2023-11-14 11:10:11 +1300

    Create error and post logs middleware

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-// Built-in middleware to handle urlendcoded data i.e form data
+// custom middleware logger
 
+app.use(logger);
+
+// Cors stands for cross origin rescource sharing
+// This array of domains are the only ones which will be allowed to recieve data from this server
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// Built-in middleware to handle urlendcoded data i.e form data
 app.use(express.urlencoded({ extended: false }));
 
 // Built-in middleware for json
@@ -52,8 +73,17 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Page Not Found'});
+    } else {
+        res.type('txt').send('404 Page Not Found');
+    }
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
